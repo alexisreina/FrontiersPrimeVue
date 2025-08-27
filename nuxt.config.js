d2e456202642c75d7cc8a359dafcecd23c1c2414
Alexis Reina <alexis.reina@frontiersin.org>
2025-08-27 15:41:37 +0200

    Squashed 'apps/showcase/' content from commit 65c5e81
    git-subtree-dir: apps/showcase
    git-subtree-split: 65c5e8114a95f9219cea112a869ff2f0945e4bd5

---
+++ nuxt.config.js
@@ -0,0 +1,98 @@
+import path from 'path';
+
+const baseUrl = '/';
+
+const alias = {
+    primevue: path.resolve(__dirname, '../../packages/primevue/src'),
+    '@primevue/core': path.resolve(__dirname, '../../packages/core/src'),
+    '@primevue/icons': path.resolve(__dirname, '../../packages/icons/src')
+};
+
+let PROCESS_ENV = {};
+
+try {
+    PROCESS_ENV = process?.env || {};
+} catch {
+    // NOOP
+}
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: false },
+    modules: ['@primevue/nuxt-module'],
+    components: [
+        {
+            path: '~/components',
+            pathPrefix: false
+        }
+    ],
+    vite: {
+        optimizeDeps: {
+            noDiscovery: true,
+            include: ['quill', 'yup']
+        },
+        resolve: {
+            dedupe: ['vue', '@primeuix/styles', '@primeuix/themes', '@primeuix/utils'],
+            alias
+        }
+    },
+    nitro: {
+        alias
+    },
+    routeRules: {
+        '/accessibility': { redirect: { to: '/guides/accessibility', statusCode: 301 } },
+        '/installation': { redirect: { to: '/vite', statusCode: 301 } }
+    },
+    primevue: {
+        usePrimeVue: process.env.DEV_ENV !== 'hot',
+        autoImport: true, // When enabled, the module automatically imports PrimeVue components and directives used throughout the application.
+        importTheme: { from: '@/themes/app-theme.js' }
+    },
+    app: {
+        baseURL: baseUrl,
+        head: {
+            title: 'PrimeVue - Vue UI Component Library',
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { name: 'description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
+                { name: 'robots', content: 'index,follow' },
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:site', content: '@primevue' },
+                { name: 'twitter:title', content: 'PrimeVue | Vue UI Component Library' },
+                { name: 'twitter:description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:title', content: 'PrimeVue | Vue UI Component Library' },
+                { property: 'og:url', content: 'https://primevue.org/' },
+                { property: 'og:description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
+                { property: 'og:image', content: 'https://www.primefaces.org/static/social/primevue-preview.jpg' },
+                { property: 'og:ttl', content: '604800' }
+            ],
+            link: [
+                { rel: 'icon', href: baseUrl + 'favicon.ico' },
+                { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' }
+            ],
+            script: [
+                {
+                    src: baseUrl + 'scripts/prism.js',
+                    'data-manual': true
+                }
+            ]
+        }
+    },
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {}
+        }
+    },
+    runtimeConfig: {
+        public: {
+            contextPath: baseUrl,
+            DEV_ENV: PROCESS_ENV.DEV_ENV,
+            designerApiUrl: ''
+        }
+    },
+    css: ['primeicons/primeicons.css', '@/assets/styles/flags.css', '@docsearch/css/dist/style.css', '@/assets/styles/tailwind/main.css', '@/assets/styles/layout/layout.scss']
+});
---
+++ pages/theming/unstyled/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <Head>
+            <Title>Unstyled - PrimeVue</Title>
+            <Meta name="description" content="Theming PrimeVue with alternative styling approaches." />
+        </Head>
+
+        <div class="doc">
+            <div class="doc-main">
+                <div class="doc-intro">
+                    <h1>Unstyled Mode</h1>
+                    <p>Theming PrimeVue with alternative styling approaches.</p>
+                </div>
+                <DocSections :docs="docs" />
+            </div>
+            <DocSectionNav :docs="docs" />
+        </div>
+    </div>
+</template>
+
+<script>
+import ArchitectureDoc from '@/doc/theming/unstyled/ArchitectureDoc.vue';
+import ExampleDoc from '@/doc/theming/unstyled/ExampleDoc.vue';
+import GlobalDoc from '@/doc/theming/unstyled/GlobalDoc.vue';
+import SetupDoc from '@/doc/theming/unstyled/SetupDoc.vue';
+import VoltDoc from '@/doc/theming/unstyled/VoltDoc.vue';
+
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    id: 'architecture',
+                    label: 'Architecture',
+                    component: ArchitectureDoc
+                },
+                {
+                    id: 'setup',
+                    label: 'Setup',
+                    component: SetupDoc
+                },
+                {
+                    id: 'example',
+                    label: 'Example',
+                    component: ExampleDoc
+                },
+                {
+                    id: 'global',
+                    label: 'Global',
+                    component: GlobalDoc
+                },
+                {
+                    id: 'volt',
+                    label: 'Volt',
+                    component: VoltDoc
+                }
+            ]
+        };
+    }
+};
+</script>
