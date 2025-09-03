bd3f5bbd688cb301f399a7b767a5353203b01476
Alexis Reina <alexis.reina@frontiersin.org>
2025-09-03 13:47:29 +0200

    feat: tooltip component styling (#14)

--- packages/preset/src/components/tooltip.ts
+++ packages/preset/src/components/tooltip.ts
@@ -1,3 +1,26 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/tooltip/index.ts
-import tooltip from "@primeuix/themes/lara/tooltip"
-export default tooltip
+import type { TooltipTokenSections } from "@primeuix/themes/types/tooltip"
+
+export const root: TooltipTokenSections.Root = {
+  maxWidth: "18rem",
+  gutter: "0.25rem",
+  padding: "0.65rem 0.5rem",
+  borderRadius: "{border.radius.xs}",
+}
+
+export const colorScheme: TooltipTokenSections.ColorScheme = {
+  light: {
+    root: {
+      background: "{surface.900}",
+      color: "{surface.0}",
+    },
+  },
+  dark: {
+    root: {
+      background: "{surface.700}",
+      color: "{surface.0}",
+    },
+  },
+}
+
+export default { root, colorScheme }
