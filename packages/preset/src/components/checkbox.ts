f99c116898ad304842ce0f3516a259e145aadf92
PrzemyslawMatlega <46319292+PrzemyslawMatlega@users.noreply.github.com>
2025-09-03 13:53:33 +0200

    Feature/inputs (#15)
    * wip
    
    * feat(inputs): add styling

--- packages/preset/src/components/checkbox.ts
+++ packages/preset/src/components/checkbox.ts
@@ -1,23 +1,2 @@
-// https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/checkbox/index.ts
-import type { CheckboxTokenSections } from "@primeuix/themes/types/checkbox"
-
-export const root: CheckboxTokenSections.Root = {
-  hoverBorderColor: "{input.hoverBorderColor}",
-  checkedBackground: "{input.checked.background}",
-  checkedHoverBackground: "{input.checked.hover.background}",
-  checkedBorderColor: "{input.checked.border.color}",
-  checkedHoverBorderColor: "{input.checked.hover.border.color}",
-  checkedFocusBorderColor: "{input.checked.focus.border.color}",
-}
-
-export const icon: CheckboxTokenSections.Icon = {
-  size: "0.75rem",
-  sm: {
-    size: "0.5rem",
-  },
-  lg: {
-    size: "1rem",
-  },
-}
-
-export default { root, icon }
+import checkbox from "@primeuix/themes/lara/checkbox"
+export default checkbox
--- packages/preset/src/components/inputtext.ts
+++ packages/preset/src/components/inputtext.ts
@@ -1,3 +1,10 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/inputtext/index.ts
 import inputtext from "@primeuix/themes/lara/inputtext"
 export default inputtext
+
+// // Notes:
+// 1. Edited paddings to match the design system
+// 2. Missing key for disabled state border color
+// 3. Missing key for error state bg color
+// 4. Missing key for disabled+filled state bg color
+// 5. It is not possible to set white focus ring for dark value
