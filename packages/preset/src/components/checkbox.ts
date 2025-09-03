e998e9b4b670c033a5a98d6b3ea42b24caef92b5
Przemyslaw Matlega <przemyslaw.matlega@frontiersin.org>
2025-09-03 14:00:36 +0200

    revert checkbox

--- packages/preset/src/components/checkbox.ts
+++ packages/preset/src/components/checkbox.ts
@@ -1,2 +1,23 @@
-import checkbox from "@primeuix/themes/lara/checkbox"
-export default checkbox
+// https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/checkbox/index.ts
+import type { CheckboxTokenSections } from "@primeuix/themes/types/checkbox"
+
+export const root: CheckboxTokenSections.Root = {
+  hoverBorderColor: "{input.hoverBorderColor}",
+  checkedBackground: "{input.checked.background}",
+  checkedHoverBackground: "{input.checked.hover.background}",
+  checkedBorderColor: "{input.checked.border.color}",
+  checkedHoverBorderColor: "{input.checked.hover.border.color}",
+  checkedFocusBorderColor: "{input.checked.focus.border.color}",
+}
+
+export const icon: CheckboxTokenSections.Icon = {
+  size: "0.75rem",
+  sm: {
+    size: "0.5rem",
+  },
+  lg: {
+    size: "1rem",
+  },
+}
+
+export default { root, icon }
