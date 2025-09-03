2656b8dfb946ec3630c35682514095e7a61cc99a
Przemyslaw Matlega <przemyslaw.matlega@frontiersin.org>
2025-09-03 13:43:47 +0200

    feat(inputs): add styling

--- packages/preset/src/components/inputtext.ts
+++ packages/preset/src/components/inputtext.ts
@@ -1,50 +1,10 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/inputtext/index.ts
-import type {
-  InputTextDesignTokens,
-  InputTextTokenSections,
-} from "@primeuix/themes/types/inputtext"
+import inputtext from "@primeuix/themes/lara/inputtext"
+export default inputtext
 
-export const root: InputTextTokenSections.Root = {
-  background: "{surface.0}",
-  disabledBackground: "{surface.300}",
-  filledBackground: "{surface.0}",
-  filledHoverBackground: "{surface.0}",
-  filledFocusBackground: "{surface.0}",
-  borderColor: "{surface.300}",
-  hoverBorderColor: "{surface.300}",
-  focusBorderColor: "{surface.300}",
-  invalidBorderColor: "{red.600}",
-  color: "{surface.950}",
-  disabledColor: "{surface.700}",
-  placeholderColor: "{surface.300}",
-  invalidPlaceholderColor: "{red.600}",
-  shadow: "none",
-  paddingX: "1rem",
-  paddingY: "0.8125rem",
-  borderRadius: "0.5rem",
-  focusRing: {
-    width: "0.125rem",
-    style: "solid",
-    color: "{blue.600}",
-    offset: "0.125rem",
-    shadow: "none",
-  },
-  // transitionDuration: "{form.field.transition.duration}",
-  // sm: {
-  //   fontSize: "{form.field.sm.font.size}",
-  //   paddingX: "{form.field.sm.padding.x}",
-  //   paddingY: "{form.field.sm.padding.y}",
-  // },
-  // lg: {
-  //   fontSize: "{form.field.lg.font.size}",
-  //   paddingX: "{form.field.lg.padding.x}",
-  //   paddingY: "{form.field.lg.padding.y}",
-  // },
-}
-
-export default {
-  root,
-} satisfies InputTextDesignTokens
-
-// Notes:
+// // Notes:
 // 1. Edited paddings to match the design system
+// 2. Missing key for disabled state border color
+// 3. Missing key for error state bg color
+// 4. Missing key for disabled+filled state bg color
+// 5. It is not possible to set white focus ring for dark value
