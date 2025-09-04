17715f415598523a95ff1a9a05e362438af3931a
Alexis Reina <alexis.reina@frontiersin.org>
2025-09-04 10:24:20 +0200

    feat: radiobutton component styling

--- packages/preset/src/components/radiobutton.ts
+++ packages/preset/src/components/radiobutton.ts
@@ -1,3 +1,23 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/radiobutton/index.ts
-import radiobutton from "@primeuix/themes/lara/radiobutton"
-export default radiobutton
+import type { RadioButtonTokenSections } from "@primeuix/themes/types/radiobutton"
+
+export const root: RadioButtonTokenSections.Root = {
+  hoverBorderColor: "{input.hoverBorderColor}",
+  checkedBackground: "{input.checked.background}",
+  checkedHoverBackground: "{input.checked.hover.background}",
+  checkedBorderColor: "{input.checked.border.color}",
+  checkedHoverBorderColor: "{input.checked.hover.border.color}",
+  checkedFocusBorderColor: "{input.checked.focus.border.color}",
+}
+
+export const icon: RadioButtonTokenSections.Icon = {
+  size: "0.75rem",
+  sm: {
+    size: "0.5rem",
+  },
+  lg: {
+    size: "1rem",
+  },
+}
+
+export default { root, icon }
