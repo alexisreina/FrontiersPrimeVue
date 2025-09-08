9dbd509477645599a49c56934a5d25f6ffb94e4d
Alexis Reina <alexis.reina@frontiersin.org>
2025-09-08 17:56:03 +0200

    feat: tag component styling (#26)
    Updated base primary colors to match figma

--- packages/preset/src/components/tag.ts
+++ packages/preset/src/components/tag.ts
@@ -1,3 +1,77 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/tag/index.ts
-import tag from "@primeuix/themes/lara/tag"
-export default tag
+import type { TagTokenSections } from "@primeuix/themes/types/tag"
+
+export const root: TagTokenSections.Root = {
+  fontWeight: "400",
+}
+
+export const colorScheme: TagTokenSections.ColorScheme = {
+  light: {
+    primary: {
+      background: "{blue.300}",
+      color: "{text.color}",
+    },
+    secondary: {
+      background: "{surface.200}",
+      color: "{text.color}",
+    },
+    success: {
+      background: "{green.300}",
+      color: "{text.color}",
+    },
+    info: {
+      background: "{cyan.300}",
+      color: "{text.color}",
+    },
+    warn: {
+      background: "{orange.300}",
+      color: "{text.color}",
+    },
+    danger: {
+      background: "{red.300}",
+      color: "{text.color}",
+    },
+    contrast: {
+      background: "{purple.300}",
+      color: "{text.color}",
+    },
+  },
+  dark: {
+    primary: {
+      background: "{blue.300}",
+      color: "{text.color}",
+    },
+    secondary: {
+      background: "{surface.200}",
+      color: "{text.color}",
+    },
+    success: {
+      background: "{green.300}",
+      color: "{text.color}",
+    },
+    info: {
+      background: "{cyan.300}",
+      color: "{text.color}",
+    },
+    warn: {
+      background: "{orange.300}",
+      color: "{text.color}",
+    },
+    danger: {
+      background: "{red.300}",
+      color: "{text.color}",
+    },
+    contrast: {
+      background: "{purple.300}",
+      color: "{text.color}",
+    },
+  },
+}
+
+export const css: TagTokenSections.CSS = `
+  .p-tag {
+    line-height: 1rem;
+  }
+`
+
+export default { root, colorScheme, css }
