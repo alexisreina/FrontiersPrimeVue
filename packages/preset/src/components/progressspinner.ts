38700c18e484550daf9b7ddaa76dc5ee932f95df
Alexis Reina <alexis.reina@frontiersin.org>
2025-09-08 17:22:38 +0200

    feat: progress spinner component styling (#25)

--- packages/preset/src/components/progressspinner.ts
+++ packages/preset/src/components/progressspinner.ts
@@ -1,3 +1,23 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/progressspinner/index.ts
-import progressspinner from "@primeuix/themes/lara/progressspinner"
-export default progressspinner
+import type { ProgressSpinnerTokenSections } from "@primeuix/themes/types/progressspinner"
+
+export const colorScheme: ProgressSpinnerTokenSections.ColorScheme = {
+  light: {
+    root: {
+      colorOne: "{primary.500}",
+      colorTwo: "{primary.500}",
+      colorThree: "{primary.500}",
+      colorFour: "{primary.500}",
+    },
+  },
+  dark: {
+    root: {
+      colorOne: "{primary.400}",
+      colorTwo: "{primary.400}",
+      colorThree: "{primary.400}",
+      colorFour: "{primary.400}",
+    },
+  },
+}
+
+export default { colorScheme }
