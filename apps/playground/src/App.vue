077f5412e24ce58a6951a764fab3fd15be13d319
Przemyslaw Matlega <przemyslaw.matlega@frontiersin.org>
2025-09-09 13:01:08 +0200

    feat(breadcrumb): fix build and example

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div class="card flex justify-center">
-    <Breadcrumb :home="home" :model="items">
-      <template #item="{ item, props }">
-        <router-link
-          v-if="item.route"
-          v-slot="{ href, navigate }"
-          :to="item.route"
-          custom>
-          <a :href="href" v-bind="props.action" @click="navigate">
-            <span :class="[item.icon, 'text-color']" />
-            <span class="text-primary font-semibold">{{ item.label }}</span>
-          </a>
-        </router-link>
-        <a v-else :href="item.url" :target="item.target" v-bind="props.action">
-          <span class="text-surface-700 dark:text-surface-0">{{
-            item.label
-          }}</span>
-        </a>
-      </template>
-    </Breadcrumb>
+    <Breadcrumb :home="home" :model="items" />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue"
+import Breadcrumb from "primevue/breadcrumb"
 
 const home = ref({
   icon: "pi pi-home",
