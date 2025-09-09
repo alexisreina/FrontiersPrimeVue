cfa34fb38f291b234e172145fbe8bf224d83b41a
Przemyslaw Matlega <przemyslaw.matlega@frontiersin.org>
2025-09-09 12:42:16 +0200

    feat(breadcrumb): add styling and icon package

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,28 +1,37 @@
-<script setup lang="ts">
-import Avatar from "primevue/avatar"
-import AvatarGroup from "primevue/avatargroup" //Optional for grouping
-import OverlayBadge from "primevue/overlaybadge"
-</script>
-
 <template>
-  <div class="p-12">
-    <OverlayBadge value="4" severity="danger" class="inline-flex">
-      <Avatar
-        class="p-overlay-badge"
-        image="https://primefaces.org/cdn/primevue/images/organization/walter.jpg"
-        size="xlarge" />
-    </OverlayBadge>
-
-    <Avatar
-      label="JD"
-      class="flex items-center justify-center mr-2"
-      size="xlarge" />
-
-    <Avatar
-      class="mr-2"
-      size="xlarge"
-      label="JD"
-      image="https://primefaces.org/cdn/primevue/images/organization/walter.jpg"
-      shape="circle" />
+  <div class="card flex justify-center">
+    <Breadcrumb :home="home" :model="items">
+      <template #item="{ item, props }">
+        <router-link
+          v-if="item.route"
+          v-slot="{ href, navigate }"
+          :to="item.route"
+          custom>
+          <a :href="href" v-bind="props.action" @click="navigate">
+            <span :class="[item.icon, 'text-color']" />
+            <span class="text-primary font-semibold">{{ item.label }}</span>
+          </a>
+        </router-link>
+        <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+          <span class="text-surface-700 dark:text-surface-0">{{
+            item.label
+          }}</span>
+        </a>
+      </template>
+    </Breadcrumb>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue"
+
+const home = ref({
+  icon: "pi pi-home",
+  route: "/introduction",
+})
+const items = ref([
+  { label: "Components" },
+  { label: "Form" },
+  { label: "InputText", route: "/inputtext" },
+])
+</script>
--- packages/preset/src/components/breadcrumb.ts
+++ packages/preset/src/components/breadcrumb.ts
@@ -1,3 +1,49 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/breadcrumb/index.ts
-import breadcrumb from "@primeuix/themes/lara/breadcrumb"
-export default breadcrumb
+import type { BreadcrumbTokenSections } from "@primeuix/themes/lara/breadcrumb"
+
+export const root: BreadcrumbTokenSections.Root = {
+  padding: "1.25rem",
+  background: "transparent",
+  gap: "0rem",
+  transitionDuration: "{transition.duration}",
+}
+
+export const item: BreadcrumbTokenSections.Item = {
+  color: "{breadcrumb.text.color}",
+  hoverColor: "{breadcrumb.text.color}",
+  borderRadius: "0.25rem",
+  icon: {
+    color: "{breadcrumb.text.color}",
+    hoverColor: "{breadcrumb.text.color}",
+  },
+}
+
+export const separator: BreadcrumbTokenSections.Separator = {
+  color: "{breadcrumb.text.color}",
+}
+
+export const css: BreadcrumbTokenSections.CSS = ({ dt }) => `
+.p-breadcrumb-item-link{
+  padding: 0 0.25rem;
+}
+.p-breadcrumb-item-link:hover {
+ background-color: ${dt("breadcrumb.background.hover.color")};
+}
+.p-breadcrumb-item{
+  font-size: 0.75rem;
+}
+.p-breadcrumb-separator-icon, .p-breadcrumb-item-icon {
+ width: 0.75rem;
+ height: 0.75rem;
+ font-size: 0.75rem;
+}
+`
+
+export default {
+  root,
+  item,
+  separator,
+  css,
+}
+
+// 1. PrimeVue breadcrumbs does not change form in mobile version by default.
