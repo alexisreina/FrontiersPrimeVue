83d646481d60574eae2d23bcec717dc1ce13f30e
Alexis Reina <alexis.reina@frontiersin.org>
2025-09-12 13:32:22 +0200

    feat: progressbar component styling

--- packages/preset/src/components/progressbar.ts
+++ packages/preset/src/components/progressbar.ts
@@ -1,3 +1,21 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/progressbar/index.ts
-import progressbar from "@primeuix/themes/lara/progressbar"
-export default progressbar
+import type { ProgressBarTokenSections } from "@primeuix/themes/types/progressbar"
+
+export const root: ProgressBarTokenSections.Root = {
+  height: "0.5rem",
+}
+
+export const value: ProgressBarTokenSections.Value = {
+  background:
+    "repeating-linear-gradient(45deg, {primary-color} 0, {primary-color} 0.5rem, color-mix(in srgb, {primary-color} 72%, white 28%) 0.5rem, color-mix(in srgb, {primary-color} 72%, white 28%) 1rem)",
+}
+
+export const label: ProgressBarTokenSections.Label = {
+  color: "transparent",
+}
+
+export default {
+  root,
+  value,
+  label,
+}
