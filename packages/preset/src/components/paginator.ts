28ffe5de8dc37a8ed17d64c2f38b6a8a624f51c2
PrzemyslawMatlega <46319292+PrzemyslawMatlega@users.noreply.github.com>
2025-09-12 10:33:57 +0200

    feat: paginator (#34)

--- packages/preset/src/components/paginator.ts
+++ packages/preset/src/components/paginator.ts
@@ -1,3 +1,42 @@
 // https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/lara/paginator/index.ts
-import paginator from "@primeuix/themes/lara/paginator"
-export default paginator
+import { PaginatorTokenSections } from "@primeuix/themes/lara/paginator"
+
+export const root: PaginatorTokenSections.Root = {
+  padding: "0",
+  gap: "0.25rem",
+  borderRadius: "0",
+  background: "transparent",
+  color: "{gray.800}",
+}
+
+export const navButton: PaginatorTokenSections.NavButton = {
+  background: "transparent",
+  hoverBackground: "{gray.200}",
+  selectedBackground: "{gray.200}",
+  color: "{gray.800}",
+  hoverColor: "{gray.900}",
+  selectedColor: "{gray.900}",
+  width: "2.5rem",
+  height: "2.5rem",
+  borderRadius: "50%",
+}
+
+export const css: PaginatorTokenSections.CSS = ({ dt }) => `
+  .p-paginator-pages{
+   margin: 0 1rem;
+  }
+  .p-paginator-pages,
+  .p-paginator-prev,
+  .p-paginator-next,
+  .p-paginator-first,
+  .p-paginator-last{
+    font-weight: 700;
+    font-size: 0.875rem;
+  }
+`
+
+export default {
+  root,
+  navButton,
+  css,
+}
